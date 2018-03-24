37d456ad93c1fe579fb03c4ebcf650f1f511c039
alexbushby <bushbya@mcmaster.ca>
2018-03-23 20:26:55 -0400

    much nicer code uploaded.

--- Project/MATH4MB_Project_RCode.R
+++ Project/MATH4MB_Project_RCode.R
@@ -6,39 +6,52 @@ library(dplyr)
 ####################HOMOGENEOUSLY-MIXED MODEL######################
 ###################################################################
 
-{parameters1 <- c(b = 0.5, y=0.3)
-state1 <- c(S = 0.998, I = 0.002)
-Eq1 <- function(t, state1, parameters1){
-  with(as.list(c(state1,parameters1)),{
-    dS <- -b*S*I
-    dI <- b*S*I - y*I
-    list(c(dS,dI))
-  })
+out1 <- function(b,y=0.3){
+  parameters1 <- function(b, y = 0.3){
+    X<- c(b,y)
+    return(X)
+  }
+  state1 <- c(S = 0.998, I = 0.002)
+  Eq1 <- function(t, state1, parameters1){
+    with(as.list(c(state1,parameters1)),{
+      dS <- -b*S*I
+      dI <- b*S*I - y*I
+      list(c(dS,dI))
+    })
+  }
+  times1 <- seq(0, 150, by = 0.01)
+  out1 <- ode(y=state1, times = times1, 
+              func = Eq1, parms = parameters1)
+  return(out1)
 }
-times1 <- seq(0, 150, by = 0.01)
-out1 <- ode(y=state1, times = times1, 
-            func = Eq1, parms = parameters1)}
 
-##1/R_0 is 0.6
-timeR<-out1[which(out1[,2] >0.599 & out1[,2]<0.601)]
-median(timeR) ##and what time the maximum occurs
+####################BETA=0.5####################
+b = 0.5
 
+M <- max(out1(b)[,3]) ##maximum percentage of infected individuals (low)
+a <- out1(b)[which(out1(b)[,3]> M - 0.000001 & out1(b)[,3] <  M + 0.000001),]
+
+####################PLOTTING FOR BETA=0.5####################
 par(mfrow=c(1,2))
 
-plot(out1[,1],out1[,2], type = "l", ylim = c(0,1), xlab="Time (Days)", ylab = "Susceptible", col = 1)
+plot(out1(b)[,1],out1(b)[,2], type = "l", ylim = c(0,1), xlab="Time (Days)", ylab = "Susceptible", col = 1, lwd = 2)
 title(main = "$\beta$ = 0.5")
 
-plot(out1[,1],out1[,3], type = "l", ylim = c(0,0.1), xlab = "Time (Days)", ylab = "Infected")
+plot(out1(b)[,1],out1(b)[,3], type = "l", ylim = c(0, M+0.03), xlab = "Time (Days)", ylab = "Infected", lwd = 2, col = 1)
 title(main = "$\beta$ = 0.5")
-abline(v=27.23)
+segments(x0=a[1,1], y0=0, x1=a[1,1], y1=a[1,3], col = 1)
 
-plot(out2[,1],out2[,3] + out2[,2], type="l", col = "blue", xlab = "Time (Days)", ylab = "Total Susceptible")
 
 ###################################################################
 ####################HETEROGENEOUSLY-MIXED MODEL####################
 ###################################################################
-{parameters2<-c(b1 = 0.6, b2 = 0.4, y = 0.3)
-state2 <- c(S1 = 0.213, S2 = 0.785, I1 = 0.001, I2 = 0.001)
+
+out2 <- function(b1,b2,y=0.3){
+  parameters2 <- function(b1, b2, y = 0.3){
+    X<- c(b1,b2,y)
+    return(X)
+  }
+  state2 <- c(S1 = 0.213, S2 = 0.785, I1 = 0.001, I2 = 0.001)
 Eq2 <- function(t, state2, parameters2){
   with(as.list(c(state2,parameters2)),{
     dS1 <- -b1*S1*(I1+I2)
@@ -48,50 +61,48 @@ Eq2 <- function(t, state2, parameters2){
     list(c(dS1, dS2, dI1, dI2))
   })
 }
-times2 <- seq(0, 150, by = 0.01)
-out2 <- ode(y=state2, times = times2, 
-            func = Eq2, parms = parameters2)}
+  times2 <- seq(0, 150, by = 0.01)
+  out2 <- ode(y=state2, times = times2, 
+              func = Eq2, parms = parameters2(b1=b1,b2=b2))
+  return(out2)
+}
 
-max(out2[,4]) ##maximum percentage of infected individuals (high)
-out2[which(out2[,4]>0.01534906 & out2[,4] < 0.01534908),]
-##time is approx 33.23
+####################BETA1=0.6 & BETA2=0.4####################
+b1 = 0.6
+b2 = 0.4
 
-max(out2[,5]) ##maximum percentage of infected individuals (low)
-out2[which(out2[,5]>0.04307889 & out2[,5] < 0.0430790),]
-##time is approx 34.50
+M1 <- max(out2(b1,b2)[,4]) ##maximum percentage of infected individuals (high contact)
+a1 <- out2(b1,b2)[which(out2(b1,b2)[,4]>M1-0.0000001 & out2(b1,b2)[,4] < M1 + 0.0000001),]
 
+M2 <- max(out2(b1,b2)[,5]) ##maximum percentage of infected individuals (low contact)
+a2 <- out2(b1,b2)[which(out2(b1,b2)[,5]>M2 -0.0000001 & out2(b1,b2)[,5] < M2 + 0.0000001),]
+
+#################PLOTTING FOR BETA1=0.6 & BETA2=0.4#################
 par(mfrow=c(1,2))
 
-plot(out2[,1],out2[,3], type = "l", ylim = c(0,1), xlab="Time (Days)", ylab = "Susceptible", col = 1)
-lines(out2[,1],out2[,2], type="l", col = 2)
-lines(out2[,1],out2[,3] + out2[,2], type="l", col = 4)
-legend("topright", c("High", "Low", "Total"), col = c(1, 2, 4), lty = c(1,1,1))
-title(main ="$\beta_1$ = 0.6,$\beta_2$ = 0.4")
+plot(out2(b1,b2)[,1],out2(b1,b2)[,3], type = "l", ylim = c(0,1), xlab="Time (Days)", ylab = "Susceptible", col = "mediumorchid", lwd = 2)
+lines(out2(b1,b2)[,1],out2(b1,b2)[,2], type="l", col = "mediumblue", lwd = 2)
+lines(out2(b1,b2)[,1],out2(b1,b2)[,3] + out2(b1,b2)[,2], type="l", col = "cyan3", lwd = 2)
+legend("topright", c("Total", "Low Contact", "High Contact"), col = c("cyan3", "mediumorchid", "mediumblue"), lwd = c(2,2,2))
+#title(main ="$\beta_1$ = 0.6,$\beta_2$ = 0.4")
 
-plot(out2[,1],out2[,5], type = "l", ylim = c(0,0.08), xlab = "Time (Days)", ylab = "Infected", xlim=c(0,80))
-lines(out2[,1],out2[,4], type="l", col = "red")
-lines(out2[,1],out2[,5] + out2[,4], type="l", col = "blue")
-legend("topright", c("High", "Low", "Total"), col = c(1, 2, 4), lty = c(1,1,1))
-title(main = "$\beta_1$ = 0.9,$\beta_2$ = 0.1")
-segments(x0=33.23, y0=0, x1=33.23, y1=0.01534907, col = "red")
-segments(x0=34.50, y0=0, x1=34.50, y1=0.0430789)
+plot(out2(b1,b2)[,1],out2(b1,b2)[,5], type = "l", ylim = c(0,max(M1,M2)+0.03), xlab = "Time (Days)", ylab = "Infected", col = "mediumorchid", lwd=2)
+lines(out2(b1,b2)[,1],out2(b1,b2)[,4], type="l", col = "mediumblue", lwd = 2)
+lines(out2(b1,b2)[,1],out2(b1,b2)[,5] + out2(b1,b2)[,4], type="l", col = "cyan3", lwd =2)
+legend("topright", c("Total", "Low Contact", "High Contact"), col = c("cyan3", "mediumorchid", "mediumblue"), lwd = c(2,2,2))
+##title(main = "$\beta_1$ = 0.9,$\beta_2$ = 0.1")
+segments(x0=a1[1,1], y0=0, x1=a1[1,1], y1=a1[1,4], col = "mediumblue", lwd = 0.5)
+segments(x0=a2[1,1], y0=0, x1=a2[1,1], y1=a2[1,5], col = "mediumorchid", lwd = 0.5)
 
+#################PLOTTING FOR BETA1=0.6 & BETA2=0.4 & BETA=0.5#################
+plot(out2(b1,b2)[,1],out2(b1,b2)[,3] + out2(b1,b2)[,2], type = "l", ylim = c(0,1), xlab="Time (Days)", ylab = "Susceptible", col = "cyan3", lwd = 2)
+lines(out1(b)[,1],out1(b)[,2], type = "l",col = 1, lwd = 2)
+legend("topright", c("Mixed", "Single"), col = c("cyan3", 1), lwd = c(2,2))
 
-##Comparing both models
+plot(out2(b1,b2)[,1],out2(b1,b2)[,5] + out2(b1,b2)[,4], type = "l", ylim = c(0,max(max(M1,M2)+0.03,M+0.03)), xlab = "Time (Days)", ylab = "Infected", col = "cyan3", lwd=2)
+lines(out1(b)[,1],out1(b)[,3], type = "l",  lwd = 2, col = 1)
+legend("topright", c("Mixed", "Single"), col = c("cyan3", 1), lwd = c(2,2))
 
-plot(out2[,1],out2[,3] + out2[,2], type = "l", ylim = c(0,1), xlab="Time (Days)", ylab = "Susceptible", col = 1)
-lines(out1[,1],out1[,2], type = "l", col = 2)
-legend("topright", c("Mixed", "Singular"), col = c(1, 2), lty = c(1,1))
-title(main ="$\beta$ = 0.5,$\beta_1$ = 0.6,$\beta_2$ = 0.4")
-lines(x=)
-
-plot(out2[,1],out2[,4] + out2[,5], type = "l", ylim = c(0,0.1), xlab="Time (Days)", ylab = "Infected", col = 1)
-lines(out1[,1],out1[,3], type = "l", col = 2)
-legend("topright", c("Mixed", "Singular"), col = c(1, 2), lty = c(1,1))
-
-plot(out2[,1],out2[,3] + out2[,2], type="l", col = "blue", xlab = "Time (Days)", ylab = "Total Susceptible")
-
-plot(out2[,1],out2[,5] + out2[,4], type = "l", col = "blue", ylim = c(0,0.05), xlab = "Time (Days)", ylab = "Total Infected")
 
 ###################################################################
 ###########################FINAL SIZE#############################
